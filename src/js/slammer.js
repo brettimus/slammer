5c55f45901c26829e2ec2906420888c33faee3db
brettimus <brbeut@gmail.com>
2015-09-26 19:18:07 -0700

    refactor several internal slammer methods

--- src/js/slammer.js
+++ src/js/slammer.js
@@ -9,9 +9,16 @@
   A constant cannot share its name with a function or a variable in the same scope.
  */
 
-const extend = require('./utils').extend;
 const Hammer = require('hammerjs');
 const SlammerNav = require("./nav");
+
+const extend         = require('./utils').extend;
+const getTransformPercentAsNumber = require("./utils").getTransformPercentAsNumber;
+const mergeClassList = require("./utils").mergeClassList;
+const mergeStyles    = require("./utils").mergeStyles;
+const newDiv         = require("./utils").newDiv;
+const setTransform   = require("./utils").setTransform;
+const setTransition  = require("./utils").setTransition;
 
 const activeSlideClass = "slam-item-active";
 const slidePositions = ["prev", "center", "next"];
@@ -29,16 +36,15 @@ class Slammer {
     
     this.options = extend({}, defaults, options);
 
-    this.wrapper = wrapperElt;
-    this.slides  = [].slice.call(this.wrapper.children, 0);
+    this.slides  = [].slice.call(wrapperElt.children, 0);
 
-    // Global lock. (?? Short circuits event handlers while a transition is occurring.)
-    this.locked = true;
+    // Global lock. (?? I think this short circuits event handlers _while_ a transition is occurring.)
+    this.lock();
 
     this.curr = 0;
     this.prev = this.slides.length - 1;
     this.next = 1;
-    
+
     this.newSlammer = null;
     this.position = null;
 
@@ -48,13 +54,14 @@ class Slammer {
 
     this.nav = null;
 
-    this.slam();
+    this.slam(wrapperElt);
   }
 
   createNav() {
     let options = this.options;
     this.nav = new SlammerNav(this, options);
     this.wrapper.appendChild(this.nav.elt);
+    return this;
   }
 
   relativeTransition(offset) {
@@ -91,151 +98,175 @@ class Slammer {
 
   retreat() {
     let newIndex = this.curr - 1;
-
     this.transformTo(this.curr, newIndex, this.options.transitionTime);
   }
 
   advance() {
     let newIndex = this.curr + 1;
-
     this.transformTo(this.curr, newIndex, this.options.transitionTime);
   }
 
   injectNewSurroundingSlides(currIndex, newIndex) {
-    const actualNewIndex = (newIndex + this.slides.length)%this.slides.length;
-    const actualNextIndex = (actualNewIndex + 1 + this.slides.length)%this.slides.length;
-    const actualPrevIndex = (actualNewIndex - 1 + this.slides.length)%this.slides.length;
+    newIndex        = this.indexify(newIndex);
+    const nextIndex = this.indexify(newIndex + 1);
+    const prevIndex = this.indexify(newIndex - 1);
 
-    let newCurrContent = this.slides[actualNewIndex].innerHTML;
-    let newNextContent = this.slides[actualNextIndex].innerHTML;
-    let newPrevContent = this.slides[actualPrevIndex].innerHTML;
+    // Update current index
+    this.curr = newIndex;
 
-    this.currSlide.innerHTML = newCurrContent;
-    this.nextSlide.innerHTML = newNextContent;
-    this.prevSlide.innerHTML = newPrevContent;
+    // Update current, next, and prev slides
+    this.currSlide.innerHTML = this.getSlideHTML(newIndex);
+    this.nextSlide.innerHTML = this.getSlideHTML(nextIndex);
+    this.prevSlide.innerHTML = this.getSlideHTML(prevIndex);
 
-    this.curr = actualNewIndex;
-
-    this.transformTo(actualNewIndex, 0, 0);
+    // Apply proper transforms to slides
+    this.transformTo(newIndex, 0, 0);
     this.nav.update(this.curr);
 
+    return this;
   }
 
   transformTo(currIndex, nextIndex, time) {
-    let currTransformPos = this.newSlammer.style.transform;
-    let currTransformContent = parseFloat(currTransformPos.split('(')[1].split('%')[0]);
 
-    let newTransformPos = 0;
+    let currTransformContent = getTransformPercentAsNumber(this.newSlammer.style.transform)
+
+    let offset = nextIndex - currIndex;
+    let isCurrentItemLast = currIndex === this.slides.length - 1;
 
     let transitionTime = this.options.transitionTime;
 
+    if (nextIndex === -1) debugger; /* ?? What causes this case ?? I couldn't trigger it. (BB) */
+
+    let newTransformPos;
     if (time <= 0) {
-      newTransformPos = (1/3)*-100;
-    } else if (nextIndex > currIndex || currIndex === this.slides.length - 1 && nextIndex === -1) {
-      newTransformPos = currTransformContent + (1/3)*-100;
-    } else {
-      newTransformPos = currTransformContent + (1/3)*100;
+      newTransformPos = (1/3) * -100;
+    }
+    else if (offset > 0 || isCurrentItemLast && nextIndex === -1) {
+      newTransformPos = currTransformContent + (1/3) * -100;
+    }
+    else {
+      newTransformPos = currTransformContent + (1/3) * 100;
     }
 
-    if (time > 0){
-      this.locked = true;
+    if (time > 0) {
+      let transitionProperty = 'transform ' + transitionTime/1000 + 's';
+      this.lock();
       this.newSlammer.classList.add('slammer-transitioning');
-      this.newSlammer.style.transition = 'transform ' + transitionTime/1000 + 's';
-      this.newSlammer.style.WebkitTransition = '-webkit-transform ' + transitionTime/1000 + 's';
+      setTransition(this.newSlammer, transitionProperty)
+
+      // TODO - call this function when the transition end event fires instead of using setTimeout
+      // (although, transitionend event has spotty browser support...)
       window.setTimeout(() => {
+
         this.newSlammer.classList.remove('slammer-transitioning');
-        this.newSlammer.style.transition = 'transform ' + 0 + 's';
-        this.newSlammer.style.WebkitTransition = '-webkit-transform ' + 0 + 's';
-        this.injectNewSurroundingSlides(currIndex, nextIndex);
-        this.locked = false;
+        setTransition(this.newSlammer, 'transform 0s')
+
+        this
+          .injectNewSurroundingSlides(currIndex, nextIndex)
+          .unlock();
+
       }, transitionTime);
-    } else if (time < 0){
+
+    } 
+    else if (time < 0){
       this.curr = 0;
     }
 
-    this.newSlammer.style.WebkitTransform = "translateX(" + newTransformPos + "%)";
-    this.newSlammer.style.transform = "translateX(" + newTransformPos + "%)";
+    setTransform(this.newSlammer, "translateX(" + newTransformPos + "%)")
+    return this;
   }
 
   acceptHammers() {
     let hammer = new Hammer(this.newSlammer);
+
     hammer.on('swipe', (e) => {
-      if (!this.locked) {
-        if (e.direction === 2) {
-          this.advance();
-        }
-        else if (e.direction === 4) {
-          this.retreat();
-        }
-      }
-    });
-    hammer.on('tap', (e) => {
-      if (!this.locked) {
+      if (this.isLocked) return;
+
+      if (e.direction === 2) {
         this.advance();
       }
+      if (e.direction === 4) {
+        this.retreat();
+      }
+    });
+
+    hammer.on('tap', (e) => {
+      if (this.isLocked()) return;
+
+      this.advance();
     })
+
+    return this;
   }
 
 
   /* Initializes a new slammer. */
-  slam() {
+  slam(wrapperElt) {
 
-    if (this.slides.length < 2) {
-      return;
-    }
+    if (this.slides.length < 2) return;
 
     // create new, three-slide slammer out of curr, prev, and next
-    this.newSlammer = document.createElement('div');
-    this.prevSlide = document.createElement('div');
-    this.currSlide = document.createElement('div');
-    this.nextSlide = document.createElement('div');
+    this.newSlammer = newDiv();
+    this.newSlammer.classList.add('slam-items');
+
+    this.prevSlide = newDiv();
+    this.currSlide = newDiv();
+    this.nextSlide = newDiv();
 
     let slides = [this.prevSlide, this.currSlide, this.nextSlide];
-    let origSlideCopies = [];
 
-    for (let i = 0; i < this.slides.length; i++) {
-      let curr = this.slides[i];
-      let newSlide = {
-        "content": curr.innerHTML,
-        "classes": curr.classList,
-        "style": curr.style
-      };
-      origSlideCopies.push(newSlide);
-    }
+    slides.forEach((slide, i) => {
 
-    for (let i = 0; i < slides.length; i++) {
-      this.newSlammer.appendChild(slides[i]);
-      let slideIndexToUse = i - 1 >= 0 ? i - 1 : origSlideCopies.length - 1;
-      slides[i].innerHTML = origSlideCopies[slideIndexToUse].content;
-      for (let j = 0; j < origSlideCopies[slideIndexToUse].classes.length; j++) {
-        slides[i].classList.add(origSlideCopies[slideIndexToUse].classes[j]);
-      }
-      for (let prop in origSlideCopies[slideIndexToUse].style) {
-        if (origSlideCopies[slideIndexToUse].style[prop] && origSlideCopies[slideIndexToUse].style[prop].length > 0) {
-          slides[i].style[prop] = origSlideCopies[slideIndexToUse].style[prop];
-        }
-      }
-    }
+      this.newSlammer.appendChild(slide);
 
-    const realWrapper = this.wrapper.parentNode;
+      let origSlideIndex = i - 1 >= 0 ? i - 1 : this.slides.length - 1; // why?
 
-    realWrapper.removeChild(this.wrapper);
+      let origSlide = this.slides[origSlideIndex];
 
-    this.wrapper = realWrapper;
+      slide.innerHTML = origSlide.innerHTML;
+      mergeClassList(slide, origSlide);
+      mergeStyles(slide, origSlide);
+    });
 
+
+    this.wrapper = wrapperElt.parentNode;
+    this.wrapper.removeChild(wrapperElt);
     this.wrapper.appendChild(this.newSlammer);
-    this.newSlammer.classList.add('slam-items');
-    this.newSlammer.style.WebkitTransform = "translateX(0%)";
-    this.newSlammer.style.transform = "translateX(0%)";
 
-    this.transformTo(-1, 0, -1);
-    this.acceptHammers();
+    setTransform(this.newSlammer, "translateX(0%)");
 
-    this.createNav();
-
-    this.locked = false;
+    this
+      .transformTo(-1, 0, -1)
+      .acceptHammers()
+      .createNav()
+      .unlock();
 
     return;
+  }
+
+  // Make sure that index is in our slide range
+  indexify(index) {
+    while (index < 0) index += this.slides.length;
+    return index % this.slides.length;
+  }
+
+  getSlideHTML(index) {
+    return this.slides[index].innerHTML;
+  }
+
+  // The `lock` interface.
+  lock() {
+    this.locked = true;
+    return this;
+  }
+
+  unlock() {
+    this.locked = false;
+    return this;
+  }
+
+  isLocked() {
+    return this.locked;
   }
 }
 
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,11 @@
 module.exports = {
     extend: extend,
+    getTransformPercentAsNumber: getTransformPercentAsNumber,
+    mergeClassList: mergeClassList,
+    mergeStyles: mergeStyles,
+    newDiv: newDiv,
+    setTransform: setTransform,
+    setTransition: setTransition,
 };
 
 /*
@@ -18,4 +24,53 @@ function extend() {
   });
 
   return result;
+}
+
+/*
+ * Parses a transform string and extracts the percentage by which it has been translated.
+ */
+function getTransformPercentAsNumber(transform) {
+  return parseFloat(transform.split('(')[1].split('%')[0]);
+}
+
+/*
+ * Sets transform property on given elt
+ */
+function setTransform(elt, transform) {
+  elt.style.WebkitTransform = transform;
+  elt.style.transform       = transform;
+}
+
+/*
+ * Sets transition property on given elt
+ */
+function setTransition(elt, transition) {
+  elt.style.WebkitTransition = transition;
+  elt.style.transition       = transition;
+}
+
+/*
+ * Merges all classes from elt2 into elt1 
+ */
+function mergeClassList(elt1, elt2) {
+  [].forEach.call(elt2.classList, (className) => {
+    elt1.classList.add(className);
+  })
+}
+
+/*
+ * Merges all styles from elt2 into elt1 
+ */
+function mergeStyles(elt1, elt2) {
+  for (let prop in elt2.style) {
+    if (elt2.style.hasOwnProperty(prop)) {
+      if (elt2.style[prop] && elt2.style[prop].length) { // Don't copy over blank style rules
+        elt1.style[prop] = elt2.style[prop];        
+      }
+    }
+  }
+}
+
+function newDiv() {
+  return document.createElement('div');
 }
